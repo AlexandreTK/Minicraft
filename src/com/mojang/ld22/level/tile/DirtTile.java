ca1192cb12e8d90978e611cdb75ae35606f5f31f
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-10-12 12:46:32 -0300

    Applying "atomic functions decompositions" on DirtTile.java

--- src/com/mojang/ld22/level/tile/DirtTile.java
+++ src/com/mojang/ld22/level/tile/DirtTile.java
@@ -25,32 +25,40 @@ public class DirtTile extends Tile {
 		screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0);
 	}
 
+	private boolean haveShovelorHoe(Item item, Player player, Level level, int xt, int yt){
+		ToolItem tool = (ToolItem) item;
+		if (tool.type == ToolType.shovel) {
+			if (player.payStamina(4 - tool.level)) {
+				level.setTile(xt, yt, Tile.hole, 0);
+				level.add(new ItemEntity(new ResourceItem(Resource.dirt), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
+				Sound.monsterHurt.play();
+				return true;
+			} else{
+				//nothing to do
+			}
+		} else {
+			//player don't have a shovel
+		}
+		if (tool.type == ToolType.hoe) {
+			if (player.payStamina(4 - tool.level)) {
+				level.setTile(xt, yt, Tile.farmland, 0);
+				Sound.monsterHurt.play();
+				return true;
+			}else {
+				//nothing to do
+			}
+		} else {
+			//player don't have a hoe
+		}
+		return false;
+	}
+	
+	
+	
+	
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
 		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.hole, 0);
-					level.add(new ItemEntity(new ResourceItem(Resource.dirt), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
-					Sound.monsterHurt.play();
-					return true;
-				} else{
-					//nothing to do
-				}
-			} else {
-				//player don't have a shovel
-			}
-			if (tool.type == ToolType.hoe) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.farmland, 0);
-					Sound.monsterHurt.play();
-					return true;
-				}else {
-					//nothing to do
-				}
-			} else {
-				//player don't have a hoe
-			}
+			haveShovelorHoe(item, player, level, xt, yt);
 		}
 		return false;
 	}
