f60b6d6ddbefb748353eb26cbb80997a45b6b090
alexandretk <alexandrekry@gmail.com>
2016-10-31 23:16:37 -0200

    Adding comments to entity/Slime.java - part 1

--- src/com/mojang/ld22/entity/Slime.java
+++ src/com/mojang/ld22/entity/Slime.java
@@ -5,38 +5,70 @@ import com.mojang.ld22.gfx.Screen;
 import com.mojang.ld22.item.ResourceItem;
 import com.mojang.ld22.item.resource.Resource;
 
+/**
+ * <h1> Slime </h1>
+ * This class is related to the Slime entity that is rendered in screen.
+ * It is responsible for the Slime Creation, Movement, Attack, Death and
+ * also to the Items it will drop.
+ * 
+ * It extends the Class Mob
+ * 
+ */
 public class Slime extends Mob {
-	private int positionXAbsolute, positionYAbsolute; //moment position of slime
-	private int jumpTime = 0; //delay of slime move
+	private int positionXAbsolute, positionYAbsolute; // Position of slime
+	private int jumpTime = 0; //delay of slime movement
 	private int lvl;
 
+	/**
+	 * This is a constructor for the slime.
+	 *
+	 * @param lvl : Indicates the level of the Slime to be created. It should 
+	 * be between 1 and 4
+	 * positionX, positionY : indicates the Slime position in the game.
+	 * health : Indicates the Slime health.
+	 * @return none.
+	 */
 	public Slime(int lvl) {
 		this.lvl = lvl;
-		positionX = random.nextInt(64 * 16);
-		positionY = random.nextInt(64 * 16);
-		health = maxHealth = lvl * lvl * 5;
+		positionX = random.nextInt(64 * 16); // Slime X position will be located between 0 and (64 * 16)
+		positionY = random.nextInt(64 * 16); // Slime Y position will be located between 0 and (64 * 16)
+		health = maxHealth = lvl * lvl * 5; 
 	}
 
+	/**
+	 * This is a method  used to control the logic updates of the game.
+	 * It basically indicates when each action will take place in the game.
+	 * 
+	 * It's related to the FPS(Frames Per Second), but while the last indicates how many times the
+	 * screen will be refreshed, the Tick indicates how many times the logic of the game will
+	 * be updated per second.
+	 *
+	 * @param none.
+	 * @return none.
+	 */
 	public void tick() {
 		super.tick();
 
 		int speed = 1;
 		if (!move(positionXAbsolute * speed, positionYAbsolute * speed) || random.nextInt(40) == 0) {
 			if (jumpTime <= -10) {
-				positionXAbsolute = (random.nextInt(3) - 1);
-				positionYAbsolute = (random.nextInt(3) - 1);
+				
+				// Slime movement when it's not following the player
+				positionXAbsolute = (random.nextInt(3) - 1); // Slime will move between -1(away) and 1 unit from the Player
+				positionYAbsolute = (random.nextInt(3) - 1); // Slime will move between -1(away) and 1 unit from the Player
 
 				if (level.player != null) {
+					//Distance Walked from the player
 					int positionXWalked = level.player.positionX - positionX;
 					int positionYWalked = level.player.positionY - positionY;
 					
-					if (positionXWalked * positionXWalked + positionYWalked * positionYWalked < 50 * 50) {
+					// After following the player for 50*50 units, act different
+					int distanceWalked = positionXWalked * positionXWalked + positionYWalked * positionYWalked;
+					if (distanceWalked < 50 * 50) {
 						if (positionXWalked < 0){
 							positionXAbsolute = -1;}
-						
 						if (positionXWalked > 0){
 							positionXAbsolute = +1;}
-						
 						if (positionYWalked < 0){
 							positionYAbsolute = -1;}
 						if (positionYWalked > 0){
@@ -49,7 +81,9 @@ public class Slime extends Mob {
 					//nothing to do
 				}
 
-				if (positionXAbsolute != 0 || positionYAbsolute != 0) jumpTime = 10;
+				if (positionXAbsolute != 0 || positionYAbsolute != 0) {
+					jumpTime = 10;
+				}
 			}else{
 				//nothing to do
 			}
@@ -65,21 +99,34 @@ public class Slime extends Mob {
 			//nothing to do
 		}
 	}
-
+	/**
+	 * This method is used to control how the game behaves when a Slime dies. That is, 
+	 * when it's attacked by the player and its life is decreased to zero.
+	 * 
+	 * Items are dropped and the player score increases.
+	 * 
+	 * @param none.
+	 * @return none.
+	 */
 	protected void die() {
 		super.die();
 
-		int count = random.nextInt(2) + 1;
+		// Indicates the number of items will be dropped
+		int count = random.nextInt(2) + 1; // value between 1 and 2
 		for (int i = 0; i < count; i++) {
-			level.add(new ItemEntity(new ResourceItem(Resource.slime), positionX + random.nextInt(11) - 5, positionY + random.nextInt(11) - 5));
+			// When the Slime dies new items are created and dropped.
+			ResourceItem resourceItem = new ResourceItem(Resource.slime);
+			int resourcePositionX = positionX + random.nextInt(11) - 5;// Item dropped at the position X of the player +- 5
+			int resourcePositionY = positionY + random.nextInt(11) - 5;// Item dropped at the position Y of the player +- 5
+			level.add(new ItemEntity(resourceItem, resourcePositionX, resourcePositionY));
 		}
 
+		// Increase player score
 		if (level.player != null) {
 			level.player.score += 25*lvl;
 		}else{
 			//nothing to do
 		}
-		
 	}
 
 	public void render(Screen screen) {
