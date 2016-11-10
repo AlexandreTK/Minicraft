6cb51bf7d31b87473b5de8f0e054a84f07a8438f
joaosaliba <joao_salibaa@hotmail.com>
2016-11-09 22:00:40 -0200

    Declaaring variables as late as possivle in Entity

--- src/com/mojang/ld22/entity/Entity.java
+++ src/com/mojang/ld22/entity/Entity.java
@@ -10,11 +10,6 @@ import com.mojang.ld22.level.tile.Tile;
 
 public class Entity {
 	protected final Random random = new Random();
-	public int positionX = 0, positionY = 0;
-	public int positionXRelative = 6;
-	public int positionYRelative = 6;
-	public boolean isRemoved;
-	public Level level;
 
 	public void render(Screen screen) {
 	}
@@ -22,19 +17,25 @@ public class Entity {
 	public void tick() {
 	}
 
+	public boolean isRemoved;
+
 	public void remove() {
 		isRemoved = true;
 	}
+
+	public Level level;
 
 	public final void init(Level level) {
 		this.level = level;
 	}
 
+	public int positionX = 0, positionY = 0;
+	public int positionXRelative = 6;
+	public int positionYRelative = 6;
+
 	public boolean intersects(int positionX0, int positionY0, int positionX1, int positionY1) {
-		return !(positionX + positionXRelative < positionX0 ||
-				 positionY + positionYRelative < positionY0 ||
-				 positionX - positionXRelative > positionX1 ||
-				 positionY - positionYRelative > positionY1);
+		return !(positionX + positionXRelative < positionX0 || positionY + positionYRelative < positionY0
+				|| positionX - positionXRelative > positionX1 || positionY - positionYRelative > positionY1);
 	}
 
 	public boolean blocks(Entity entity) {
@@ -48,40 +49,37 @@ public class Entity {
 	}
 
 	public boolean move(int positionXAbsolute, int positionYAbsolute) {
-		
+
 		if (positionXAbsolute != 0 || positionYAbsolute != 0) {
-			
+
 			boolean isStopped = true;
-			
+
 			if (positionXAbsolute != 0 && move2(positionXAbsolute, 0))
 				isStopped = false;
-			
+
 			if (positionYAbsolute != 0 && move2(0, positionYAbsolute))
 				isStopped = false;
-			
+
 			if (!isStopped) {
 				int positionXWalked = positionX >> 4;
 				int positionYWalked = positionY >> 4;
-				level.getTile(positionXWalked, positionYWalked).steppedOn(level, positionXWalked, positionYWalked, this);
-			}
-			else {
+				level.getTile(positionXWalked, positionYWalked).steppedOn(level, positionXWalked, positionYWalked,
+						this);
+			} else {
 				// nothing to do
 			}
 			return !isStopped;
-		}
-		else {
+		} else {
 			// nothing to do
 		}
 		return true;
 	}
 
 	protected boolean move2(int positionXAbsolute, int positionYAbsolute) {
-		
+
 		if (positionXAbsolute != 0 && positionYAbsolute != 0)
 			throw new IllegalArgumentException("Move2 can only move along one axis at a time!");
 
-		
-		
 		boolean isBlocked = false;
 
 		int positionXWalked0 = 0;
@@ -92,8 +90,7 @@ public class Entity {
 		positionYWalked0 = ((positionY + positionYAbsolute) - positionYRelative) >> 4;
 		positionXWalked1 = ((positionX + positionXAbsolute) + positionXRelative) >> 4;
 		positionYWalked1 = ((positionY + positionYAbsolute) + positionYRelative) >> 4;
-		
-		
+
 		int xto0 = 0;
 		int yto0 = 0;
 		int xto1 = 0;
@@ -102,49 +99,46 @@ public class Entity {
 		yto0 = ((positionY) - positionYRelative) >> 4;
 		xto1 = ((positionX) + positionXRelative) >> 4;
 		yto1 = ((positionY) + positionYRelative) >> 4;
-		
+
 		for (int positionYWalked = positionYWalked0; positionYWalked <= positionYWalked1; positionYWalked++)
 			for (int positionXWalked = positionXWalked0; positionXWalked <= positionXWalked1; positionXWalked++) {
-				
-				if (positionXWalked >= xto0 && positionXWalked <= xto1 &&
-					positionYWalked >= yto0 && positionYWalked <= yto1)
+
+				if (positionXWalked >= xto0 && positionXWalked <= xto1 && positionYWalked >= yto0
+						&& positionYWalked <= yto1)
 					continue;
-				
-				level.getTile(positionXWalked, positionYWalked).bumpedInto(level, positionXWalked, positionYWalked, this);
-				
-				if (!level.getTile(positionXWalked, positionYWalked).mayPass(level, positionXWalked, positionYWalked, this)) {
+
+				level.getTile(positionXWalked, positionYWalked).bumpedInto(level, positionXWalked, positionYWalked,
+						this);
+
+				if (!level.getTile(positionXWalked, positionYWalked).mayPass(level, positionXWalked, positionYWalked,
+						this)) {
 					isBlocked = true;
 					return false;
-				}
-				else {
+				} else {
 					// nothing to do
 				}
 			}
-		if (isBlocked) return false;
+		if (isBlocked)
+			return false;
 
-		
-		
 		List<Entity> isInside = level.getEntities(positionX + positionXAbsolute - positionXRelative,
-												  positionY + positionYAbsolute - positionYRelative,
-												  positionX + positionXAbsolute + positionXRelative,
-												  positionY + positionYAbsolute + positionYRelative);
-		
+				positionY + positionYAbsolute - positionYRelative, positionX + positionXAbsolute + positionXRelative,
+				positionY + positionYAbsolute + positionYRelative);
+
 		for (int i = 0; i < isInside.size(); i++) {
 			Entity entity = isInside.get(i);
-			
-			if (entity == this) 
+
+			if (entity == this)
 				continue;
 
 			entity.touchedBy(this);
 		}
-		
-		List<Entity> wasInside = level.getEntities(positionX - positionXRelative,
-				   positionY - positionYRelative,
-				   positionX + positionXRelative,
-				   positionY + positionYRelative);
-		
+
+		List<Entity> wasInside = level.getEntities(positionX - positionXRelative, positionY - positionYRelative,
+				positionX + positionXRelative, positionY + positionYRelative);
+
 		isInside.removeAll(wasInside);
-		
+
 		for (int i = 0; i < isInside.size(); i++) {
 			Entity entity = isInside.get(i);
 			if (entity == this)
@@ -152,20 +146,19 @@ public class Entity {
 
 			if (entity.blocks(this)) {
 				return false;
-			}
-			else {
+			} else {
 				// nothing to do
 			}
 		}
 
 		positionX += positionXAbsolute;
 		positionY += positionYAbsolute;
-		
+
 		return true;
 	}
 
 	protected void touchedBy(Entity entity) {
-	
+
 	}
 
 	public boolean isBlockableBy(Mob mob) {
@@ -173,7 +166,7 @@ public class Entity {
 	}
 
 	public void touchItem(ItemEntity itemEntity) {
-	
+
 	}
 
 	public boolean canSwim() {
