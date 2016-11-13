a0edac797e60276c180034b4e52bf23862d1c95a
joaosaliba <joao_salibaa@hotmail.com>
2016-11-13 19:03:54 -0200

    emphasis on importate code in ItemEnity

--- src/com/mojang/ld22/entity/ItemEntity.java
+++ src/com/mojang/ld22/entity/ItemEntity.java
@@ -7,16 +7,17 @@ import com.mojang.ld22.sound.Sound;
 
 public class ItemEntity extends Entity {
 	private int lifeTime = 0; // time that player are alive
-	protected int walkedDistancy = 0; //distance that player walked
-	protected int direction = 0; //direction that player walked
-	public int hurtTime = 0; // time that player need for recover of some type of hurt
+	protected int walkedDistancy = 0; // distance that player walked
+	protected int direction = 0; // direction that player walked
+	public int hurtTime = 0; // time that player need for recover of some type
+								// of hurt
 	protected int positionXKnockback = 0, positionYKnockback = 0;
 	public double positionXAbsolute = 0.0;
 	public double positionYAbsolute = 0.0;
 	public double positionZAbsolute = 0.0;
 	public double xx = 0.0, yy = 0.0, zz = 0.0;
 	public Item item;
-	private int time = 0; //time
+	private int time = 0; // time
 
 	public ItemEntity(Item item, int x, int y) {
 		this.item = item;
@@ -38,8 +39,7 @@ public class ItemEntity extends Entity {
 		if (time >= lifeTime) {
 			remove();
 			return;
-		}
-		else {
+		} else {
 			// nothing to do
 		}
 		xx += positionXAbsolute;
@@ -50,8 +50,7 @@ public class ItemEntity extends Entity {
 			positionZAbsolute *= -0.5;
 			positionXAbsolute *= 0.6;
 			positionYAbsolute *= 0.6;
-		}
-		else {
+		} else {
 			// nothing to do
 		}
 		positionZAbsolute -= 0.15;
@@ -67,7 +66,8 @@ public class ItemEntity extends Entity {
 		xx += gotx - expectedx;
 		yy += goty - expectedy;
 
-		if (hurtTime > 0) hurtTime--;
+		if (hurtTime > 0)
+			hurtTime--;
 	}
 
 	public boolean isBlockableBy(Mob mob) {
@@ -76,17 +76,13 @@ public class ItemEntity extends Entity {
 
 	public void render(Screen screen) {
 		if (time >= lifeTime - 6 * 20) {
-			if (time / 6 % 2 == 0) return;
-		}
-		else {
+			if (time / 6 % 2 == 0)
+				return;
+		} else {
 			// nothing to do
 		}
 		screen.render(positionX - 4, positionY - 4, item.getSprite(), Color.get(-1, 0, 0, 0), 0);
 		screen.render(positionX - 4, positionY - 4 - (int) (zz), item.getSprite(), item.getColor(), 0);
-	}
-
-	protected void touchedBy(Entity entity) {
-		if (time > 30) entity.touchItem(this);
 	}
 
 	public void take(Player player) {
@@ -94,5 +90,10 @@ public class ItemEntity extends Entity {
 		player.score++;
 		item.onTake(this);
 		remove();
+	}
+
+	protected void touchedBy(Entity entity) {
+		if (time > 30)
+			entity.touchItem(this);
 	}
 }
