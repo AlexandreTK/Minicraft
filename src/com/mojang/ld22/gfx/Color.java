1298b59affdc041f2121cf5722a5fb60fb7875a9
alexandretk <alexandrekry@gmail.com>
2016-11-23 17:21:28 -0200

    Verify return values - src/com/mojang/ld22/gfx/Color.java

--- src/com/mojang/ld22/gfx/Color.java
+++ src/com/mojang/ld22/gfx/Color.java
@@ -3,7 +3,9 @@ package com.mojang.ld22.gfx;
 public class Color {
 
 	public static int get(int a, int b, int c, int d) {
-		int mixColors = (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a)); 
+		int mixColors = (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a));
+		
+		assert(mixColors >= 0) : "Color value must be positive";
 		return mixColors;
 	}
 
@@ -14,11 +16,11 @@ public class Color {
 		int colorMixture = ((int) red * 36 + green * 6 + blue);
 		
 		if (d < 0) {
-			return 255;
+			colorMixture = 255;
 		} else {
 			// Do nothing
 		}
-		
+		assert(colorMixture >= 0) : "Color value must be positive";
 		return colorMixture;
 	}
 
